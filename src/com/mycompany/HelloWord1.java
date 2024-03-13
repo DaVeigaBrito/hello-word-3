041f976d30cb39be2fe605db6092c617170c5b55
Dani.Brito <daveigacv@gmailcom>
2024-03-13 16:06:28 -0100

    wor1

---
+++ src/com/mycompany/HelloWord1.java
@@ -0,0 +1,9 @@
+package com.mycompany;
+
+public class HelloWord1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello word1!");
+        System.out.println("Hello word1");
+    }
+}
