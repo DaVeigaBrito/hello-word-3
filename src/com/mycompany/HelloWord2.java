5038f7e4ace0ad2747e5d8a6d86c8f4efdad9e4e
Dani.Brito <daveigacv@gmailcom>
2024-03-13 21:09:49 -0100

    create repository

---
+++ src/com/mycompany/HelloWord2.java
@@ -0,0 +1,9 @@
+package com.mycompany;
+
+public class HelloWord2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello word2!");
+        System.out.println("Hello word2!");
+    }
+}
