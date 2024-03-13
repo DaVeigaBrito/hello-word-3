da329ce6d0ebd4b65faf43fb6ad348a82c3d47a3
DaVeigaBrito <147720560+DaVeigaBrito@users.noreply.github.com>
2024-03-13 21:53:09 -0100

    Update HelloWord2.java
    hello2 dani 5

--- src/com/mycompany/HelloWord2.java
+++ src/com/mycompany/HelloWord2.java
@@ -7,5 +7,6 @@ public class HelloWord2 {
         System.out.println("Hello word2!");
         System.out.println("Hello word3!");
         System.out.println("Hello word41!");
+        System.out.println("Hello word51!");
     }
 }
